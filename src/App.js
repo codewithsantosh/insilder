5efd72d516a7ff656042b6ea89b600d5bce626a5
codewithsantosh <santoshpowar1997@gmail.com>
2023-03-01 09:12:53 +0530

    version 1.1

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ function App() {
         "https://res.cloudinary.com/dwzmsvp7f/image/fetch/q_75,f_auto,w_560/https%3A%2F%2Fmedia.insider.in%2Fimage%2Fupload%2Fc_crop%2Cg_custom%2Fv1675948934%2Fbfpd5nnzzpcysja1jyvu.jpg",
     },
   ];
+  
   const users1 = [
     {
       title: "Theatre & art",
@@ -56,7 +57,39 @@ function App() {
       title: "Workshops",
     },
   ];
-  const users2 = [{}, {}, {}, {}];
+  const users2 = [
+    {
+      title: "Rajaa - Live in Concert | Hyderabad | Mumbai",
+      disp1: "February 26",
+      price: "799 Onwards(Inslusive of GST)",
+      subpart: "Step out and enjoy this event",
+      imageurl:
+        "https://res.cloudinary.com/dwzmsvp7f/image/fetch/q_75,f_auto,w_560/https%3A%2F%2Fmedia.insider.in%2Fimage%2Fupload%2Fc_crop%2Cg_custom%2Fv1676353538%2Fsa0i4u6jgk2mixpetjp4.jpg",
+    },
+    {
+      title: "Arijit Singh - One Night Only Tour, Bangalore 2023",
+      disp1: "March 4",
+      price: "4500 Onwards",
+      subpart: "Step out and enjoy this event",
+      imageurl:
+        "https://res.cloudinary.com/dwzmsvp7f/image/fetch/q_75,f_auto,w_560/https%3A%2F%2Fmedia.insider.in%2Fimage%2Fupload%2Fc_crop%2Cg_custom%2Fv1676363451%2Fww3glajzxpmvabdeqian.png",
+    },
+    {
+      title: "Under 25 Summit  One Night Only Tour 2023",
+      disp1: "March4 & 5 ",
+      price: "2655 Onwards",
+      subpart: "Step out and enjoy this event",
+      imageurl:
+        "https://res.cloudinary.com/dwzmsvp7f/image/fetch/q_75,f_auto,w_560/https%3A%2F%2Fmedia.insider.in%2Fimage%2Fupload%2Fc_crop%2Cg_custom%2Fv1675948934%2Fbfpd5nnzzpcysja1jyvu.jpg",
+    }, {
+      title: "Arijit Singh - One Night Only Tour, Bangalore 2023",
+      disp1: "March 4",
+      price: "4500 Onwards",
+      subpart: "Step out and enjoy this event",
+      imageurl:
+        "https://res.cloudinary.com/dwzmsvp7f/image/fetch/q_75,f_auto,w_560/https%3A%2F%2Fmedia.insider.in%2Fimage%2Fupload%2Fc_crop%2Cg_custom%2Fv1676363451%2Fww3glajzxpmvabdeqian.png",
+    },
+  ];
   const icons = [
     {
       imgurl:
@@ -108,6 +141,8 @@ function App() {
     },
   ];
 
+
+
   return (
     <>
       <Layout1 />
@@ -142,7 +177,7 @@ function App() {
               marginLeft: "20px",
             }}
           >
-            <Compoffourcard />
+            <Compoffourcard data={item2} />
           </div>
         ))}
       </div>
@@ -154,8 +189,8 @@ function App() {
           </div>
         ))}
       </div>
-
       <Compfooter />
+      
     </>
   );
 }
--- src/components/compoffourcard.js
+++ src/components/compoffourcard.js
@@ -1,23 +1,33 @@
 import React from "react";
 import { Card } from "antd";
-const { Meta } = Card;
 
-export default function compoffourcard() {
+export default function compoffourcard(props) {
   return (
     <>
-      <Card
+      <Card className="parent"
         hoverable
         style={{
           width: 290,
         }}
         cover={
-          <img
+          <img className="child" style={{height:'300px'}}
             alt="example"
-            src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+            src={props.data.imageurl}
+            
           />
+
         }
+
       >
-        <Meta title="Europe Street beat" description="www.instagram.com" />
+        
+        <p style={{fontSize:'16px'}}>{props.data.disp1}</p>
+         <h3> {props.data.price}</h3>
+         <div style={{ backgroundColor: "#F5FCFE", padding: "10px" }}>
+          <strong style={{ color: "#31C0F0", margin: "0" }}>
+            {props.data.subpart}
+          </strong>
+        </div>
+
       </Card>
     </>
   );
